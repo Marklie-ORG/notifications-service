1d2857ca5d21ba703f452faf280cf5cfb12edada
romapchel <notpchelintsevroma@gmail.com>
2025-06-13 17:08:59 +0300

    log sending report

--- app/src/lib/classes/CommunicationWrapper.ts
+++ app/src/lib/classes/CommunicationWrapper.ts
@@ -3,6 +3,7 @@ import { SendGridService } from "../services/SendgridService.js";
 import { SlackService } from "marklie-ts-core/dist/lib/services/SlackService.js";
 import { TokenService } from "marklie-ts-core";
 import { WhapiService } from "../services/WhapiService.js";
+import {ActivityLog} from "marklie-ts-core/dist/lib/entities/ActivityLog.js";
 const database = await Database.getInstance();
 
 export class CommunicationWrapper {
@@ -24,11 +25,23 @@ export class CommunicationWrapper {
 
     if (client.emails && client.emails.length > 0) {
       for (const email of client.emails) {
+
         await this.sendGrid.sendReportEmail({
           to: email,
           subject: `Your Report Is Ready!`,
           text: 'We’ve completed your report and it is now ready for review.',
         }, report );
+
+        const log = database.em.create(ActivityLog, {
+          organizationUuid: client.organization.uuid,
+          action: 'report_sent',
+          targetType: 'report',
+          clientUuid: client.uuid,
+          actor: 'system',
+          metadata: {email: email}
+        });
+
+        await database.em.persistAndFlush(log);
       }
     }
 
@@ -40,11 +53,33 @@ export class CommunicationWrapper {
         Buffer.from(report, "base64"),
         "report.pdf"
       );
+
+      const log = database.em.create(ActivityLog, {
+        organizationUuid: client.organization.uuid,
+        action: 'report_sent',
+        targetType: 'report',
+        clientUuid: client.uuid,
+        actor: 'system',
+        metadata: {slackConversationId: client.slackConversationId}
+      });
+
+      await database.em.persistAndFlush(log);
     }
 
     if (client.phoneNumbers && client.phoneNumbers.length > 0) {
       for (const phoneNumber of client.phoneNumbers) {
         await this.whapiService.sendReportWhatsapp(report, phoneNumber);
+
+        const log = database.em.create(ActivityLog, {
+          organizationUuid: client.organization.uuid,
+          action: 'report_sent',
+          targetType: 'report',
+          clientUuid: client.uuid,
+          actor: 'system',
+          metadata: {phoneNumber: client.phoneNumbers}
+        });
+
+        await database.em.persistAndFlush(log);
       }
     }
 
